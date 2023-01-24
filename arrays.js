93600adea12ccd98986a2ace1689f09b19683427
anyango55 <anyangoc55@gmail.com>
2023-01-24 16:37:31 +0300

    Javascript Arrays

---
+++ arrays.js
@@ -0,0 +1,22 @@
+//  ARRAYS
+// Arrays and Array Methods
+    // 1 Basics of arrays
+    // 2Dynamic Functions
+    // 3 Tip Calculator App
+    // 4Basics of Arrays
+
+
+    const groceries = ['banana','apple','pear','mango','cookie','chocolate']
+    console.log(groceries)
+    // indexing
+    // console.log(groceries[1])
+    // adding
+    groceries.push('orange')
+    // slice method
+    // console.log(groceries.slice(1,3))
+    // console.log(groceries.slice(1,3
+    
+    // array methods(slice, push,indexof, length)
+    // console.log(groceries.indexOf('orange'))
+    // console.log(groceries.length) 
+    // NB LENGTH STARTS FROM 1
